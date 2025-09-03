f43e7131f84f85be8e4316ba9e24561bf1b4681b
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-03 04:38:38 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Create a new paragraph element
+const newParagraph = document.createElement('p');
+
+// Set the text content of the new paragraph
+newParagraph.textContent = 'This text was added by JavaScript.';
+
+// Append the new paragraph to the end of the body
+document.body.appendChild(newParagraph);
